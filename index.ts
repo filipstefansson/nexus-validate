9925ebd2cecbe2cd0c001dd0d9f7beedb41c336d
Filip Stefansson <filip.stefansson@gmail.com>
2021-01-22 15:43:40 +0100

    feat: add plugin

---
+++ index.ts
@@ -0,0 +1,119 @@
+import { plugin } from 'nexus';
+import {
+  ArgsValue,
+  GetGen,
+  MaybePromise,
+  printedGenTyping,
+  printedGenTypingImport,
+} from 'nexus/dist/core';
+import * as yup from 'yup';
+import { ObjectShape } from 'yup/lib/object';
+
+type Yup = typeof yup;
+
+const FieldValidateResolverImport = printedGenTypingImport({
+  module: './validation',
+  bindings: ['FieldValidateResolver'],
+});
+
+const fieldDefTypes = printedGenTyping({
+  optional: true,
+  name: 'validate',
+  description: 'Validate mutation arguments.',
+  type: 'FieldValidateResolver<TypeName, FieldName>',
+  imports: [FieldValidateResolverImport],
+});
+
+export type FieldValidateResolver<
+  TypeName extends string,
+  FieldName extends string
+> = (
+  yup: Yup,
+  args: ArgsValue<TypeName, FieldName>,
+  ctx: GetGen<'context'>
+) => MaybePromise<ObjectShape>;
+
+export interface FieldValidatePluginErrorConfig {
+  // can be of type `yup.ValidationError`
+  error: Error | yup.ValidationError;
+  args: any;
+  ctx: GetGen<'context'>;
+}
+
+export interface FieldValidatePluginConfig {
+  formatError?: (config: FieldValidatePluginErrorConfig) => Error;
+}
+
+export const defaultFormatError = ({
+  error,
+}: FieldValidatePluginErrorConfig): Error => {
+  if (error instanceof yup.ValidationError) {
+    return new ValidationError(error.message, {
+      invalidArgs: error.path ? [error.path] : [],
+    });
+  }
+
+  return error;
+};
+
+export const fieldValidationPlugin = (
+  validateConfig: FieldValidatePluginConfig = {}
+) => {
+  const { formatError = defaultFormatError } = validateConfig;
+
+  return plugin({
+    name: 'NexusValidate',
+    description: 'The validate plugin provides validation for arguments.',
+    fieldDefTypes: fieldDefTypes,
+    onCreateFieldResolver(config) {
+      const validate: FieldValidateResolver<any, any> =
+        config.fieldConfig.extensions?.nexus?.config.validate;
+
+      // if the field doesn't have an validate field, don't worry about wrapping the resolver
+      if (validate == null) {
+        return;
+      }
+
+      // if it does have this field, but it's not a function, it's wrong - let's provide a warning
+      if (typeof validate !== 'function') {
+        console.error(
+          new Error(
+            `The validate property provided to ${
+              config.fieldConfig.name
+            } with type ${
+              config.fieldConfig.type
+            } should be a function, saw ${typeof validate}`
+          )
+        );
+        return;
+      }
+
+      return async (root, args, ctx, info, next) => {
+        try {
+          const schemaBase = await validate(yup, args, ctx);
+          const schema = yup.object().shape(schemaBase);
+          await schema.validate(args);
+          return next(root, args, ctx, info);
+        } catch (error) {
+          throw formatError({ error, args, ctx });
+        }
+      };
+    },
+  });
+};
+
+class ValidationError extends Error {
+  extensions: {
+    invalidArgs: string[];
+  };
+
+  constructor(
+    message: string,
+    extensions: {
+      invalidArgs: string[];
+    }
+  ) {
+    super(message);
+    this.extensions = extensions;
+  }
+}
